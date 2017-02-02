ac59d7666f1d599c7af5958402fe1aed2afac45a
Sabbir Ahmed <sabbir0ahmed0@gmail.com>
2017-02-01 20:40:19 -0500

    I should find my other scripts too

--- git_it_all.sh
+++ git_it_all.sh
@@ -1,14 +1,16 @@
 #!/bin/zsh
 
 # git_it_all
-# A simple and lazy way to check which Git repositories in the input directory
-# has uncommitted changes
+# 
+# A simple and lazy way to resursively check which Git repositories in the
+# input directory has uncommitted changes
+# 
 # Usage:
-#   ./git_it_all.sh {DIR_NAME}
+#   ./git_it_all.sh dir_path
 
 
 function parse_git_dirty {
-  [[ "$(git status 2> /dev/null | tail -n1)" != "no changes"* ]] && echo "*"
+    [[ "$(git status 2> /dev/null | tail -n1)" != "no changes"* ]] && echo "*"
 }
 
 function go_back {
@@ -19,20 +21,19 @@ ABS_PATH=$(pwd)
 DIR_TREE=$(find $1 -type d -not -path "*.git*" -not -path "*.venv*")
 MENU_STR="Contents of $1:"
 MENU_LEN=$(expr length $MENU_STR)
+RED_TEXT="\033[33;31m"
 
 cd "$1"
-printf "$MENU_STR \n"
+printf "$MENU_STR\n"
 printf "=%.0s" {1..$MENU_LEN}
-printf "\n"
-ls -d */
-printf "\n"
+printf "\n\n$(ls -d */)\n\n"
 go_back
 
-for repo in "${(f)DIR_TREE}"
+for repo in ${(f)DIR_TREE}
 do
-    cd "$repo"
+    cd $repo
     if [[ $(parse_git_dirty) != "*" && -d ".git" ]]; then
-        echo "$repo has uncommitted changes"
+        echo -e "$RED_TEXT$repo has uncommitted changes"
     fi
     go_back
 done

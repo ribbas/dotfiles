ced9bcd8e504ea95013d29f60f93699c13176480
Sabbir Ahmed <sabbir0ahmed0@gmail.com>
2017-02-08 22:34:00 -0500

    finished py_up

--- py_up/py_up.sh
+++ py_up/py_up.sh
@@ -1,13 +1,27 @@
 #!/bin/zsh
 
+# py_up
+# 
+# A simple and lazy way to set up Python projects. This script creates a
+# virtualenv with either versions of Python interpreters, upgrades pip
+# configurations and adds enviroment helper scripts and Makefiles
+# 
+# Usage:
+#   ./py_up proj_name python_ver
+
+
 mkdir $1
 cd $1
 
-virtualenv .venv
+if [ "$2" = "2" ]; then
+    virtualenv .venv
+elif [ "$2" = "3" ]; then
+    virtualenv -p /usr/bin/python3 .venv3
+fi
+
 touch requirements.txt
+cp ~/helper-scripts/py_up/env.py ~/helper-scripts/py_up/Makefile .
 
-cp ../env.py .
-cp ../Makefile .
-
-source .venv/bin/activate
+source .venv*/bin/activate
 pip install -U pip
+deactivate

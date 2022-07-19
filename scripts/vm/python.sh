951e2264ae187a23e86ab02ff98329847b23391b
ribbas <sab61r.a@gmail.com>
2022-07-19 14:48:00 -0400

    update python version to 3.10

--- scripts/vm/python.sh
+++ scripts/vm/python.sh
@@ -4,7 +4,7 @@ set -o errexit
 py_dev () {
     # Python
     echo "Installing Python development tools..."
-    sudo apt install python3.8 python3.8-venv python3-pip python3-dev -y
-    sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.8 1
-    sudo update-alternatives --set python /usr/bin/python3.8
+    sudo apt install python3.10 python3.10-venv python3-pip python3-dev -y
+    sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.10 1
+    sudo update-alternatives --set python /usr/bin/python3.10
 }
